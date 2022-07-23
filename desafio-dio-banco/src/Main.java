44226476bdd6f231000db9bbd275c75368005fc7
Bianca Vecchi <bia.vecchi@hotmail.com>
2022-07-23 17:42:37 -0300

    Desafio Banco Digital

---
+++ desafio-dio-banco/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cliente bianca = new Cliente();
+		bianca.setNome("Bianca");
+		
+		
+		Conta cc = new ContaCorrente(bianca);
+		Conta poupanca = new ContaPoupanca(bianca);
+		
+		cc.depositar(100);
+		cc.transferir(20, poupanca);
+
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+	}
+
+}
